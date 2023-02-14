f99eff58e77bf3ee4ad72ab3f0b6561294616848
JoeGlov <125394334+JoeGlov@users.noreply.github.com>
2023-02-14 12:52:52 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "816736645415067648"; // your league ID
+export const leagueID = "916413825275138048"; // your league ID
 export const leagueName = "DFL"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
